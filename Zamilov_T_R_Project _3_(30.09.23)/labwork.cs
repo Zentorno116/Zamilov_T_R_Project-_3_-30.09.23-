26be23086ae26d17f918b9c945a06649718b8d85
Timur <zamilov05@mail.ru>
2023-10-05 20:57:19 +0300

    Добавьте файлы проекта.

---
+++ Zamilov_T_R_Project _3_(30.09.23)/labwork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zamilov_T_R_Project_3_30_09_23
+{
+    internal class labwork
+    {
+        static void Main(string[] args)
+        {
+            
+        }
